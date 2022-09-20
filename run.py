ca211744ced7c084b4b497d3ecab8c291c6f9a69
Anna Gabain <anna.gabain@outlook.com>
2022-09-20 10:04:32 +0000

    Create main function and start by greeting the user

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def main():
+    """
+    Runs when the programme starts
+    """
+    print(f"\n...............Hello and welcome to MOM DATA...............\n")
+    print(f"...Here you can get insights about your monthly expenses...\n")
+
+
+main()
